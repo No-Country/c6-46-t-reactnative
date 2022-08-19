e73d6140ad9de007483927d4f0dfea61a1da9fc8
NaFaPaz <nfanego@gmail.com>
2022-08-19 20:20:32 -0300

    [ADD] Redux, LogIn

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import { StackNav } from './navigation/navigation';
 import { DrawerNav } from './navigation/drawerNav';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <DrawerNav />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StackNav />
+      </NavigationContainer>
+    </Provider>
   );
 }
